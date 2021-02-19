6d25f5feb404b29ce3b7e1557ad7ada8321cb873
JeromeSchweitzer <jschweit@purdue.edu>
2021-02-19 18:58:04 -0500

    refactor needed

--- game.py
+++ game.py
@@ -2,14 +2,69 @@ import chess
 from time import sleep
 from random import randint
 
+def dumb_move_eval(board, move):
+    # Returns True if black has more pieces than white
+    before_white_score = (
+        len(board.pieces(1,True)) +
+        3*len(board.pieces(2,True)) +
+        3*len(board.pieces(3,True)) +
+        5*len(board.pieces(4,True)) +
+        9*len(board.pieces(5,True))
+    )
+    before_black_score = (
+        len(board.pieces(1,False)) +
+        3*len(board.pieces(2,False)) +
+        3*len(board.pieces(3,False)) +
+        5*len(board.pieces(4,False)) +
+        9*len(board.pieces(5,False))
+    )
+    before_eval = before_black_score - before_white_score
+
+    temp_board = board.copy()
+    temp_board.push(move)
+    after_white_score = (
+        len(temp_board.pieces(1,True)) +
+        3*len(temp_board.pieces(2,True)) +
+        3*len(temp_board.pieces(3,True)) +
+        5*len(temp_board.pieces(4,True)) +
+        9*len(temp_board.pieces(5,True))
+    )
+    after_black_score = (
+        len(temp_board.pieces(1,False)) +
+        3*len(temp_board.pieces(2,False)) +
+        3*len(temp_board.pieces(3,False)) +
+        5*len(temp_board.pieces(4,False)) +
+        9*len(temp_board.pieces(5,False))
+    )
+    after_eval = after_black_score - after_white_score
+
+    return after_eval - before_eval
 
 
-def random_move(board):
-    legal_moves_list = list(board.legal_moves)
-    random_int_choice = randint(0,board.legal_moves.count()-1)
-    move = legal_moves_list[random_int_choice]
+def generate_random_move(move_list):
+    random_int_choice = randint(0,len(move_list)-1)
+    move = move_list[random_int_choice]
     return move
 
+def generate_half_move_ahead(board):
+    legal_moves_list = list(board.legal_moves)
+
+    move_evals = []
+    for move in legal_moves_list:
+        move_evals.append(dumb_move_eval(board, move))
+    
+    best_eval = max(move_evals)
+
+    candidate_moves = []
+    for idx, move_eval in enumerate(move_evals):
+        if move_eval == best_eval:
+            candidate_moves.append(legal_moves_list[idx])
+    
+    best_move = generate_random_move(candidate_moves)
+
+    print("The best move is " + str(best_move) + " with a score of " + str(best_eval))
+    
+    return best_move
 
 
 board = chess.Board()
@@ -32,7 +87,8 @@ while not board.is_game_over():
     print(board)
     sleep(2)
 
-    computer_move = random_move(board)
+    # computer_move = generate_random_move(list(board.legal_moves))
+    computer_move = generate_half_move_ahead(board)
 
     print()
     print(computer_move)
