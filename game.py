5355932d17b5b18eca5f94daffb7cc6664097fda
JeromeSchweitzer <jschweit@purdue.edu>
2021-02-21 13:51:37 -0500

    improved code quality

--- game.py
+++ game.py
@@ -1,15 +1,14 @@
 import chess
 from time import sleep
-from random import randint
+from random import choice
 
 
-# Hyperparameters
+# Constants
 PAWN_WEIGHT=1
 KNIGHT_WEIGHT=3
 BISHOP_WEIGHT=3
 ROOK_WEIGHT=5
 QUEEN_WEIGHT=9
-NUM_MOVES_AHEAD=2
 
 
 def game_loop(board):
@@ -24,59 +23,40 @@ def game_loop(board):
 
         board.push(user_move)
         print(board)
-        # sleep(1)
 
         if board.is_game_over():
             break
+        
+        computer_move, computer_move_score = recursive_minimax(board, num_half_moves_ahead=2)
+        print(f"Computer move chosen as {computer_move}, with a score of {computer_move_score}.")
 
-        # computer_move = generate_random_move(list(board.legal_moves))
-        # computer_move = generate_half_move_ahead(board)
-        # computer_move = generate_minimax_move(board)
-        computer_move, computer_move_eval = recursive_minimax(board, NUM_MOVES_AHEAD)
-        print(f"Computer move chosen as {computer_move}, with a score of {computer_move_eval}.")
-
-        # print(f"\n{computer_move}\n")
         board.push(computer_move)
         print(board)
 
 
 def generate_half_move_ahead(board, maximize=True):
-    legal_moves_list = list(board.legal_moves)
+    legal_moves = dict.fromkeys(board.legal_moves)
 
-    move_evals = []
-    for move in legal_moves_list:
-        move_evals.append(dumb_move_eval(board.copy(), move))
+    for move in legal_moves.keys():
+        analysis_board = board.copy()
+        legal_moves[move] = dumb_move_score(analysis_board, move)
     
-    if len(move_evals) == 0:
-        print("no move evals?")
-        print(board)
-    
-    best_eval = max(move_evals) if maximize else min(move_evals)
+    best_score = max(legal_moves.values()) if maximize else min(legal_moves.values())
 
-    candidate_moves = []
-    for idx, move_eval in enumerate(move_evals):
-        if move_eval == best_eval:
-            candidate_moves.append(legal_moves_list[idx])
-    
-    best_move = generate_random_move(candidate_moves)
+    candidate_moves = [move for move,score in legal_moves.items() if score==best_score]
 
-    # print(f"\nThe best move is {best_move} with a score of {best_eval}")
+    best_move = choice(candidate_moves)
     
-    return (best_move, best_eval)
+    return (best_move, best_score)
 
 
 def recursive_minimax(board, num_half_moves_ahead) -> (chess.Move, int):
-    # if num_half_moves_ahead = 0, pass board into generate_half_move_ahead
-    # generate list of legal moves for board
-    # for each legal move, push legal move to temp_board
-    # pass temp_board into generate_minimax_move with num_half_moves_ahead-1, assign this value to 
-    # 
-    if num_half_moves_ahead == 0:
-        best_move, best_eval = generate_half_move_ahead(board, maximize=(False if board.turn==chess.WHITE else True))
-        return (best_move, best_eval)
+    if num_half_moves_ahead == 1:
+        best_move, best_score = generate_half_move_ahead(board, maximize=board.turn==chess.BLACK)
+        return (best_move, best_score)
     
     root_moves = list(board.legal_moves)
-    root_moves_evals = []
+    root_moves_scores = []
     for root_move in root_moves:
         temp_board = board.copy()
         temp_board.push(root_move)
@@ -84,73 +64,41 @@ def recursive_minimax(board, num_half_moves_ahead) -> (chess.Move, int):
         if temp_board.is_game_over():
             return (root_move, 100000 if temp_board.result()=='0-1' else -100000)
         else:
-            best_move, best_eval = recursive_minimax(temp_board, num_half_moves_ahead-1)
-            root_moves_evals.append(best_eval)
-        # except:
-        #     print("there was a checkmate somewhere.")
-        #     root_moves_evals.append(100000 if analysis_board.result()=='0-1' else -100000)
+            best_move, best_score = recursive_minimax(temp_board, num_half_moves_ahead-1)
+            root_moves_scores.append(best_score)
     
-    best_eval = max(root_moves_evals) if board.turn==chess.BLACK else min(root_moves_evals)
+    best_score = max(root_moves_scores) if board.turn==chess.BLACK else min(root_moves_score)
     candidate_moves = []
-    for idx, move_eval in enumerate(root_moves_evals):
-        if move_eval == best_eval:
+    for idx, move_score in enumerate(root_moves_scores):
+        if move_score == best_score:
             candidate_moves.append(root_moves[idx])
     
-    if num_half_moves_ahead == NUM_MOVES_AHEAD:
-        print()
-        print(root_moves)
-        print(root_moves_evals)
-        print()
-    return (generate_random_move(candidate_moves), best_eval)
+    # if num_half_moves_ahead == NUM_MOVES_AHEAD:
+    #     print()
+    #     print(root_moves)
+    #     print(root_moves_evals)
+    #     print()
+    return (choice(candidate_moves), best_score)
 
 
-def generate_minimax_move(board, num_moves_ahead=1):
-    root_moves = list(board.legal_moves)
-    root_moves_evals = []
-    for root_move in root_moves:
-        analysis_board = board.copy()
-        analysis_board.push(root_move)
-        opp_moves = list(analysis_board.legal_moves)
-        best_opp_eval = generate_half_move_ahead(analysis_board, max=False)
-        root_moves_evals.append(best_opp_eval)
-    best_eval = max(root_moves_evals)
-    candidate_moves = []
-    for idx, move_eval in enumerate(root_moves_evals):
-        if move_eval == best_eval:
-            candidate_moves.append(root_moves[idx])
-    
-    best_move = generate_random_move(candidate_moves)
-    return best_move
-
-
-def generate_random_move(move_list):
-    random_int_choice = randint(0,len(move_list)-1)
-    move = move_list[random_int_choice]
-    return move
-
-
-def dumb_move_eval(analysis_board, move):
-    # before_white_score = count_material(analysis_board, chess.WHITE)
-    # before_black_score = count_material(analysis_board, chess.BLACK)
-    # before_eval = before_black_score - before_white_score
-
+def dumb_move_score(analysis_board, move):
     analysis_board.push(move)
     after_white_score = count_material(analysis_board, chess.WHITE)
     after_black_score = count_material(analysis_board, chess.BLACK)
-    after_eval = after_black_score - after_white_score
+    after_score = after_black_score - after_white_score
     if analysis_board.is_game_over():
         return 100000 if analysis_board.result()=='0-1' else -100000
 
-    return after_eval
+    return after_score
 
 
-def count_material(board, color):
+def count_material(analysis_board, color):
     material_count = (
-        PAWN_WEIGHT*len(board.pieces(chess.PAWN,color)) +
-        KNIGHT_WEIGHT*len(board.pieces(chess.KNIGHT,color)) +
-        BISHOP_WEIGHT*len(board.pieces(chess.BISHOP,color)) +
-        ROOK_WEIGHT*len(board.pieces(chess.ROOK,color)) +
-        QUEEN_WEIGHT*len(board.pieces(chess.QUEEN,color))
+        PAWN_WEIGHT*len(analysis_board.pieces(chess.PAWN,color)) +
+        KNIGHT_WEIGHT*len(analysis_board.pieces(chess.KNIGHT,color)) +
+        BISHOP_WEIGHT*len(analysis_board.pieces(chess.BISHOP,color)) +
+        ROOK_WEIGHT*len(analysis_board.pieces(chess.ROOK,color)) +
+        QUEEN_WEIGHT*len(analysis_board.pieces(chess.QUEEN,color))
     )
 
     return material_count
