92bb2f6e160456056ed20903b556317bc9356099
JeromeSchweitzer <jschweit@purdue.edu>
2021-02-24 18:11:14 -0500

    ab pruning working, needs refactor

--- game.py
+++ game.py
@@ -28,7 +28,10 @@ def game_loop(board):
         
         recursive_calls = 0
         before_time = perf_counter()
-        computer_move, computer_move_evl = recursive_minimax(board, depth=3)
+
+        # computer_move, computer_move_evl = minimax(board, depth=4)
+        computer_move, computer_move_evl = alpha_beta_minimax(board, depth=4, alpha=-99999999, beta=99999999)
+
         after_time = perf_counter()
 
         print(f"Computer move chosen as {computer_move}, with an evaluation of {computer_move_evl}.")
@@ -38,7 +41,7 @@ def game_loop(board):
         print(board)
 
 
-# TODO: Implement alpha beta pruning
+# TODO: Refactor this function
 # Notes on Alpha Beta pruning
 # 
 # alpha represents the minimum score white is assured,
@@ -49,11 +52,47 @@ def game_loop(board):
 # nodes cease to be explored when the maximum score that black is assured is
 # is less than the minimum score that white is assured
 # 
-def recursive_minimax_ab(board, depth, alpha, beta) -> (chess.Move, int):
-    pass
+def alpha_beta_minimax(board, depth, alpha, beta) -> (chess.Move, int):
+    global recursive_calls  #   Used for debugging/useful stats
+    recursive_calls += 1
+
+    if depth == 0:
+        evl = dumb_evl(board)
+        return (None, evl)
+    
+    root_moves = dict.fromkeys(board.legal_moves)
+    for move in root_moves:
+        analysis_board = board.copy()
+        analysis_board.push(move)
+
+        if analysis_board.is_game_over():   # Don't need to continue here if game over
+            evl = 100000 if analysis_board.result()=='1-0' else -100000 if analysis_board.result()=='0-1' else 0
+            if board.turn == chess.WHITE:
+                alpha = max(alpha, evl)
+            else:
+                beta = min(beta, evl)
+            return (move, evl)
+        
+        best_move, minimax_evl = alpha_beta_minimax(analysis_board, depth-1, alpha, beta)
+        root_moves[move] = minimax_evl
+        
+        if board.turn == chess.WHITE:
+            alpha = max(alpha, minimax_evl)
+        else:
+            beta = min(beta, minimax_evl)
+        
+        if beta < alpha:
+            break
+    
+    move_evls = [evl for evl in root_moves.values() if evl is not None]
+
+    best_evl = max(move_evls) if board.turn==chess.WHITE else min(move_evls)
+    candidate_moves = [move for move,evl in root_moves.items() if evl==best_evl]
+    
+    return (choice(candidate_moves), best_evl)
 
 
-def recursive_minimax(board, depth) -> (chess.Move, int):
+def minimax(board, depth) -> (chess.Move, int):
     global recursive_calls  #   Used for debugging/useful stats
     recursive_calls += 1
     
@@ -69,7 +108,7 @@ def recursive_minimax(board, depth) -> (chess.Move, int):
         if analysis_board.is_game_over():   # Don't need to continue here if game over
             return (move, 100000 if analysis_board.result()=='1-0' else -100000 if analysis_board.result()=='0-1' else 0)
         
-        best_move, minimax_evl = recursive_minimax(analysis_board, depth-1)
+        best_move, minimax_evl = minimax(analysis_board, depth-1)
         root_moves[move] = minimax_evl
     
     best_evl = max(root_moves.values()) if board.turn==chess.WHITE else min(root_moves.values())
@@ -78,6 +117,8 @@ def recursive_minimax(board, depth) -> (chess.Move, int):
     return (choice(candidate_moves), best_evl)
 
 
+# TODO: Take the following into account:
+#       pins, castling, doubled pawns, any other positional stuff
 def dumb_evl(analysis_board):
     # "dumb" because just counting weighted material
     if analysis_board.is_game_over():   # If game is over, don't bother
